34ab9c99bc41157374b3a493327d809535ff9553
Bia Coelho <biacoelho@pm.me>
2021-01-13 03:15:37 -0300

    Add Sidebar component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from "../Profile"
-
-import * as S from './styled.js'
-import GlobalStyles from '../../styles/global.js'
+import Sidebar from "../Sidebar"
+import * as S from './styled'
+import GlobalStyles from '../../styles/global'
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
   )
---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const SidebarWrapper = styled.aside`
+  background: #192734;
+  border-right: 1px solid #38444d;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  position: fixed;
+  padding: 2rem;
+  text-align: center;
+  width: 20rem;
+`
