7396310795044cd8ec1bacea7d0b4a4468bb8110
Bia Coelho <biacoelho@pm.me>
2021-01-16 18:27:35 -0300

    Add SocialLinks component

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Linktree } from "@styled-icons/simple-icons/Linktree"
+
+const Icons = {
+  Github,
+  Youtube,
+  Linkedin,
+  Linktree
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/biacoelho`,
+  },
+  {
+    label: "Youtube",
+    url: `https://www.youtube.com/channel/UCJVvmqhXnxpkdqHNSIQAHlg`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/biacoelho`,
+  },
+  {
+    label: "Linktree",
+    url: `https://linktr.ee/biacoelho`,
+  }
+]
+
+export default links
