af94a383b464ebdbd06ef78d0b701f92470e42b0
Bia Coelho <biacoelho@pm.me>
2021-01-18 00:47:47 -0300

    Create props to PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,18 @@ import Layout from "../components/Layout";
 import PostItem from "../components/PostItem";
 import SEO from "../components/seo";
 
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      background="#1fa1f2"
+      category="Misc"
+      date="18 de Janeiro de 2021"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razoẽs para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
