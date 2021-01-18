4a412ebd96f590a1748abb6439383a61dc9002a9
Bia Coelho <biacoelho@pm.me>
2021-01-18 01:35:49 -0300

    Fix path folder posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/posts`,
       },
     },
     {
