ec3699f318565e2b020abd7f81f4fe9ca69382af
Bia Coelho <biacoelho@pm.me>
2021-02-06 15:53:21 -0300

    Configure search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react"
+
+import algoliasearch from "algoliasearch/lite"
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom"
+
+const algolia = {
+  appId: process.env.GATSBY_ALGOLIA_APP_ID,
+  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+}
+const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey)
 
 const Search = () => (
-  <h1>Search Bia</h1>
+  <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+    <SearchBox />
+    <Hits />
+  </InstantSearch>
 )
 
 export default Search
